1622896295b9172354cb2dc9b12933aea368e88f
raksa <eng.raksa@gmail.com>
2019-06-13 09:56:18 +0700

    update admin

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from . import models as m
 
-admin.site.register(m.Question)
-admin.site.register(m.Choice)
+
+@admin.register(m.Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text',)
+
+
+@admin.register(m.Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('choice_text', 'votes')
